3dc50b1e2ba0f5b411a75ed31a832b20e79773e1
KunZhang <kuna.zhang@gmail.com>
2017-08-20 23:19:11 -0700

    1 conv layer,3 connected NN

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ with open('../data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
-for line in lines[1:-1]:
+for line in lines[1:50]:
     # print(line)
     source_path = line[0]
     filename = source_path.split('/')[-1]  #extract file name
@@ -27,14 +27,43 @@ y_train = np.array(measurements)
 # #build a simply NN/ regression NN
 # import tensorflow
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers.core import Flatten, Dense, Lambda, Dropout, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D 
 print('create model')
-model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
-model.add(Dense(1))
 
-print('compile mode and run')
+model = Sequential()
+
+model.add(Lambda(lambda x: (x / 255.0 - 0.5), input_shape=(160,320,3)))
+
+# model.add(Convolution2D(nb_filter=16, nb_row=7, nb_col=7, \
+    # border_mode='valid', input_shape=(1, 31, 31), activation='tang'))
+
+model.add(Convolution2D(32, 32, 3, input_shape=(160, 320, 3)))
+model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+
+# model.add(Convolution2D(32, 32, 3, input_shape=(160, 320, 3)))
+# model.add(MaxPooling2D((2,2)))
+# model.add(Dropout(0.7))
+# model.add(Activation('relu'))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+
+model.add(Dense(5))
+model.add(Activation('relu'))
+
+model.add(Dense(1))
+# model.add(Activation('softmax'))
+
+# model.add(Lambda(lambda x: (x / 255.0 - 0.5), input_shape=(160,320,3)))
+# model.add(Flatten())
+# model.add(Dense(1))
+
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train, validation_split=0.2,shuffle=True,nb_epoch = 10)
+model.fit(X_train,y_train, validation_split=0.2,shuffle=True,nb_epoch =5)
 
 model.save('model.h5')
